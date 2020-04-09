81bd979eb00c1d72c734c12aef0312bdd6f03601
Yakshchopra <yakshchopra@gmail.com>
2020-04-09 20:16:14 +0530

    stats backend basics

---
+++ stats/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'Countries', component: CountriesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
