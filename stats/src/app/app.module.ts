7e708f042d77ac07d3bf56f05569e27f163acfff
harshgoel05 <harshgoel05@gmail.com>
2020-04-11 13:45:12 +0530

    merge

--- stats/src/app/app.module.ts
+++ stats/src/app/app.module.ts
@@ -1,36 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { CountriesComponent } from './components/countries/countries.component';
-import {HttpClientModule} from '@angular/common/http';
-import { AddStoreComponent } from './add-store/add-store.component';
-import { DoctordashboardComponent } from './doctordashboard/doctordashboard.component';
-import { CommonfooterComponent } from './commonfooter/commonfooter.component';
-import { DoctorloginComponent } from './doctorlogin/doctorlogin.component';
-import { StoreRegestrtionComponent } from './store-regestrtion/store-regestrtion.component'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { CountriesComponent } from "./components/countries/countries.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AddStoreComponent } from "./add-store/add-store.component";
+import { DoctordashboardComponent } from "./doctordashboard/doctordashboard.component";
+import { CommonfooterComponent } from "./commonfooter/commonfooter.component";
+import { DoctorloginComponent } from "./doctorlogin/doctorlogin.component";
+import { StoreRegestrtionComponent } from "./store-regestrtion/store-regestrtion.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    CountriesComponent,
-    AddStoreComponent,
-    DoctordashboardComponent,
-    CommonfooterComponent,
-    DoctorloginComponent,
-    StoreRegestrtionComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        CountriesComponent,
+        AddStoreComponent,
+        DoctordashboardComponent,
+        CommonfooterComponent,
+        DoctorloginComponent,
+        StoreRegestrtionComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
