fa3389018eb054bcedcce882ab1e5f796b4b7658
harshgoel05 <harshgoel05@gmail.com>
2020-04-12 20:42:02 +0530

    home fixes

---
+++ stats/src/app/components/countries/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { DataServiceService } from "./node_modules/src/app/services/data-service.service";
+
+@Component({
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+    constructor(private dataService: DataServiceService) {}
+
+    ngOnInit(): void {
+        this.dataService.getGlobalData().subscribe({
+            next: (result) => {
+                console.log(result);
+            },
+        });
+    }
+    scrollToElement($element): void {
+        console.log($element);
+        $element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    }
+}
