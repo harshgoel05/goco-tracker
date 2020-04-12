4f40fa60f58d4bd7d0c401e4ebc7fbfa129b33e9
sanskar agrawal <agrawalsanskar9993@gmail.com>
2020-04-12 22:04:51 +0530

    bedsfix

--- stats/src/app/beds/beds.component.ts
+++ stats/src/app/beds/beds.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { StoreService } from "../_services/store.service";
+import { map } from "rxjs/operators";
 @Component({
     selector: "app-beds",
     templateUrl: "./beds.component.html",
     styleUrls: ["./beds.component.css"],
 })
 export class BedsComponent implements OnInit {
-    beds: any;
+    beds: any
+    bedsarray:any
+
     constructor(private _service: StoreService) {}
 
     ngOnInit() {
         this._service.getbeds().subscribe(
             (res) => {
-                console.log(res);
-                this.beds = res;
+                this.beds=res
+                this.bedsarray=this.beds.regional
             },
             (error) => {
                 console.log("Error while getting beds database");
