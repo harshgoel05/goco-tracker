105e5db674cfa83357b347595cd5f6b3b370b669
harshgoel05 <harshgoel05@gmail.com>
2020-04-13 18:25:24 +0530

    local rem

--- stats/src/app/services/data-service.service.ts
+++ stats/src/app/services/data-service.service.ts
@@ -10,7 +10,7 @@ export class DataServiceService {
     private globalDataURL1 =
         "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-08-2020.csv";
     private IndiaDataURL = "https://api.covid19india.org/data.json";
-    private baseurl = "http://localhost:3000/api";
+    private baseurl = "/api";
     private countrywiseData = this.baseurl + "/covidWorldCountryWise";
     private globalDataURL = this.baseurl + "/covidWorld";
 
